c7e2b4624c009b6f9e7f21c487887f4b55217f38
Carlos Gonzalez <carlosigonz@gmail.com>
2021-05-11 18:01:40 -0400

    Agregado proyecto

---
+++ ProyectoOperativos1GonzalezMicale/src/proyectooperativos1gonzalezmicale/Main.java
@@ -0,0 +1,16 @@
+package proyectooperativos1gonzalezmicale;
+
+/**
+ *
+ * @author carlo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
